fa28a6d749d7327050f5c772bf04925a63783671
haozi23333 <haozi23333@gmail.com>
2017-05-08 16:48:38 +0800

    add readme.md

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,9 @@
  * Created by haozi on 5/8/2017.
  */
 import {parseMessage} from './parseMessage'
-import {textMessage} from './Message/textMessage'
 
 const messageParse = parseMessage.parse.bind(parseMessage)
 
 export {
-    messageParse,
-    textMessage
+    messageParse
 }
